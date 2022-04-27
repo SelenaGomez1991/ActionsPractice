e1b3d2380d4f4efa413cf7f95e7f1e0eb55a4d93
SelenaGomez1991 <104508792+SelenaGomez1991@users.noreply.github.com>
2022-04-27 20:05:20 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,9 @@
+import sys
 
+url = sys.argv[1]
+password  = sys.argv[2]
+bool_val = sys.argv[3]
+
+print(f'URL: {url}')
+print(f'Password: {password}')
+print(f'Bool Val: {bool_val}, {type(bool_val)}')
